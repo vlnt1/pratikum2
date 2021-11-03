13adf512810a38a677b0f33dfa3e2d5b10f2ef65
vlnt <michaellaisina47@gmail.com>
2021-11-04 02:19:18 +0700

    menambahkan file pyton kedalam github

---
+++ pratikum2.py
@@ -0,0 +1,6 @@
+phi = 3.14
+radius = int(input("masukan jari jari lingkaran : "))
+luas = phi*(radius*radius)
+keliling = 2*phi*radius
+print("luas lingkaran adalah : {} cm2".format(luas))
+print("keliling lingkaran adalah : {} cm".format(keliling))
